61abe927958ee3f5d0ef124e86057ffb0de2fd7b
Willian Maruyama <67539871+Dakarakoso@users.noreply.github.com>
2024-08-20 17:09:41 +0000

    Error test

--- pages/index.js
+++ pages/index.js
@@ -2,4 +2,4 @@ function Home () {
     return <h1>Raquel, Você é o amor da minha vida. Quero passar o resto da minha vida ao seu lado. Te amo!</h1>
 }
 
-export default Home;
+export default Home;git 
