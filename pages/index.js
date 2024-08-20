4201ea0444d53562957e29a3ca874f55d0d71a2e
Willian Maruyama <67539871+Dakarakoso@users.noreply.github.com>
2024-08-20 19:53:12 +0000

    atualização da frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Raquel, Você é o amor da minha vida. Quero passar o resto da minha vida ao seu lado. Te amo!</h1>
+    return <h1>Raquel, Você é o amor da minha vida. Quero passar o resto da minha vida ao seu lado. Te amo! Se você me ama da uma risadinha</h1>
 }
 
 export default Home; 
