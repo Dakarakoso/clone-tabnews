5445c5dd8cffd512599ba87479a912b7ac00f586
Willian Maruyama <67539871+Dakarakoso@users.noreply.github.com>
2024-08-22 20:00:09 +0000

    added script ´lint:fix´ and lint:check

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # clone-tabnews
+
 https://www.tabnews.com.br/
 Project to practice coding and improve skills
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
-function Home () {
-    return <h1>Raquel, Você é o amor da minha vida. Quero passar o resto da minha vida ao seu lado. Te amo! Se você me ama da uma risadinha</h1>
+function Home() {
+  return (
+    <h1>
+      Raquel, Você é o amor da minha vida. Quero passar o resto da minha vida ao
+      seu lado. Te amo! Se você me ama da uma risadinha
+    </h1>
+  );
 }
 
-export default Home; 
+function test() {
+  console.log("teste");
+}
+
+function test2() {
+  console.log("teste2");
+}
+
+export default Home;
