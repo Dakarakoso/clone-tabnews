0e71482346e700e553e7c8442572efcb54fcf10b
dakarakoso <willian_jvm@hotmail.com>
2024-08-26 08:59:07 -0300

    added `test/integration/api/v1/status` and removed unnecessary tests

--- models/calculadora.js
+++
@@ -1,7 +0,0 @@
-const somar = (numero1, numero2) => {
-  if (typeof numero1 !== "number" || typeof numero2 !== "number")
-    return "error";
-  return numero1 + numero2;
-};
-
-exports.somar = somar;
