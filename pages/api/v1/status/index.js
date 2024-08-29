e8637d8f2474a0067ca24a6630b291f0dc423c36
dakarakoso <willian_jvm@hotmail.com>
2024-08-29 16:35:25 -0300

    feat: improve environment configuration and database setup
    - Removed `.env` file to enhance security and environment management.
    - Updated `compose.yaml` to use `env_file` for loading environment variables, improving consistency and flexibility.
    - Modified `database.js` to utilize environment variables for database connection, enhancing configurability and security.
    - Updated `package.json` to include `services:up` in the `dev` script, ensuring that required services are up before starting development.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,8 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result);
+  const result = await database.query("SELECT 1 + 1 AS sum;");
+  console.log(result.rows);
   res.status(200).json({ key: "willian" });
 }
 
