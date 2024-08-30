b761466e73dc2e5132d8c021fc3974bc5664aad4
dakarakoso <willian_jvm@hotmail.com>
2024-08-30 09:57:38 -0300

    Enhance status endpoint with database version and connection details
    - Added query to fetch PostgreSQL server version.
    - Added query to get the maximum number of connections allowed.
    - Added query to count the number of currently opened connections.
    - Updated response to include the database version, maximum connections, and number of opened connections.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,29 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 AS sum;");
-  console.log(result.rows);
-  res.status(200).json({ key: "willian" });
+  const updatedAt = new Date().toISOString();
+  const dbVersionQuery = await database.query(
+    "SELECT current_setting('server_version');",
+  );
+  const dbVersion = dbVersionQuery.rows[0].current_setting;
+  const maxConnectionsQuery = await database.query("SHOW max_connections;");
+  const maxConnections = parseInt(maxConnectionsQuery.rows[0].max_connections);
+  const openedConnectionsQuery = await database.query(
+    "SELECT COUNT(*) AS open_connections FROM pg_stat_activity;",
+  );
+  const openedConnections = parseInt(
+    openedConnectionsQuery.rows[0].open_connections,
+  );
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: maxConnections,
+        opened_connections: openedConnections,
+        version: dbVersion,
+      },
+    },
+  });
 }
 
 export default status;
