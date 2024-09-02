006abb09668e32a168c8500f3f7bef4bef42eed0
Willian Maruyama <67539871+Dakarakoso@users.noreply.github.com>
2024-09-02 13:47:58 +0000

    added console.log for debugging

--- infra/database.js
+++ infra/database.js
@@ -7,12 +7,19 @@ async function query(ObjectQuery) {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
   });
-  await client.connect();
   try {
+    console.log({
+      host: process.env.POSTGRES_HOST,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+    });
+    await client.connect();
     const result = await client.query(ObjectQuery);
     return result;
   } catch (err) {
     console.error(err);
+    throw error;
   } finally {
     await client.end();
   }
