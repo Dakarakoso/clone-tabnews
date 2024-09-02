bc6a6d09ee3b584210af83c596be23969118a8e1
dakarakoso <willian_jvm@hotmail.com>
2024-09-02 16:08:14 -0300

    Refactor SSL configuration to use a utility function and support CA certificates

--- infra/database.js
+++ infra/database.js
@@ -6,7 +6,7 @@ async function query(ObjectQuery) {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSslValue(),
   });
   console.log("credentials: ", {
     host: process.env.POSTGRES_HOST,
@@ -24,6 +24,15 @@ async function query(ObjectQuery) {
   } finally {
     await client.end();
   }
+}
+
+function getSslValue() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV === "development" ? false : false;
 }
 
 export default {
