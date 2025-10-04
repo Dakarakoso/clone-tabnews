5c72e1d624cdb314c614df236ced379392c7afa0
Dakarakoso <willian_jvm@hotmail.com>
2025-10-04 19:31:10 +0900

    feat: add default feature `read:activation_token` when creating user

--- models/user.js
+++ models/user.js
@@ -84,15 +84,16 @@ async function create(userInputValues) {
   await validateUniqueUsername(userInputValues.username);
   await validateUniqueEmail(userInputValues.email);
   await hashPasswordInObject(userInputValues);
+  injectDefaultFeaturesInObject(userInputValues);
   const newUser = await runInsertQuery(userInputValues);
   return newUser;
   async function runInsertQuery(userInputValues) {
     const results = await database.query({
       text: `
       INSERT INTO 
-        users (username, email, password) 
+        users (username, email, password, features) 
       VALUES 
-        ($1, $2, $3)
+        ($1, $2, $3, $4)
       RETURNING
        *
         ;`,
@@ -100,9 +101,13 @@ async function create(userInputValues) {
         userInputValues.username,
         userInputValues.email,
         userInputValues.password,
+        userInputValues.features,
       ],
     });
     return results.rows[0];
+  }
+  function injectDefaultFeaturesInObject(userInputValues) {
+    userInputValues.features = ["read:activation_token"];
   }
 }
 
