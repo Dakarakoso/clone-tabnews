c618420a2bebfa6b5cbc6585ccb787cce5de7983
Dakarakoso <willian_jvm@hotmail.com>
2026-01-09 18:10:36 +0900

    feat: require `create:user` to access `/api/v1/users`

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -125,4 +125,36 @@ describe("POST /api/v1/users", () => {
       });
     });
   });
+  describe("Default user", () => {
+    test("with unique and valid data", async () => {
+      const createdUser1 = await orchestrator.createUser();
+      await orchestrator.activateUser(createdUser1);
+      const sessionObject = await orchestrator.createSession(createdUser1.id);
+
+      const user2Response = await fetch("http://localhost:3000/api/v1/users", {
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: `session_id=${sessionObject.token}`,
+        },
+        method: "POST",
+        body: JSON.stringify({
+          username: "loggedUser",
+          email: "loggedUser@email.com",
+          password: "pass1234",
+        }),
+      });
+
+      expect(user2Response.status).toBe(403);
+
+      const responseBody = await user2Response.json();
+
+      expect(responseBody).toEqual({
+        name: "ForbiddenError",
+        message: "You do not have permission to perform this action.",
+        action:
+          "Please contact support if you believe this is an error. Or check if you the create:user permission",
+        statusCode: 403,
+      });
+    });
+  });
 });
--- tests/integration/infra/email.test.js
+++ tests/integration/infra/email.test.js
@@ -23,7 +23,6 @@ describe("infra/email.js", () => {
     });
 
     const lastEmail = await orchestrator.getLastEmail();
-    console.log(lastEmail);
     expect(lastEmail.sender).toBe("<contato@fintab.com.br>");
     expect(lastEmail.recipients[0]).toBe("<contato@curso.dev>");
     expect(lastEmail.subject).toBe("Last email");
