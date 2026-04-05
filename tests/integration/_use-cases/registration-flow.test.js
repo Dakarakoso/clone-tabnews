5c94b5c9fec87c34d87f9a5bc543bbf1b63a8b88
Dakarakoso <willian_jvm@hotmail.com>
2026-04-05 11:15:16 +0900

    fix: add correct email to pass the tests

--- tests/integration/_use-cases/registration-flow.test.js
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -45,7 +45,7 @@ describe("Use case: Registration Flow(all successful)", () => {
   test("Receive activation email", async () => {
     const lastEmail = await orchestrator.getLastEmail();
 
-    expect(lastEmail.sender).toBe("<contato@fintab.com.br>");
+    expect(lastEmail.sender).toBe("<contato@maruyama-work.com.br>");
     expect(lastEmail.recipients[0]).toBe("<registration.flow@curso.dev>");
     expect(lastEmail.subject).toBe("Activate your account");
     expect(lastEmail.text).toContain("RegistrationFlow");
